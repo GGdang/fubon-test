9f02e1854059ca2f7e3d6e6a45caa02a222f8be9
Star-Bit-Hardy <hardy.chen@star-bit.io>
2021-09-15 17:15:44 +0800

    搬移 header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,5 @@
 <template>
   <v-app class="fubon-test">
-    <v-img sizes="" src="@/assets/img/image_background.svg">
-      <template v-slot:defaut>
-        <div>
-          <v-app-bar app color="agba(0,0,0,0)" fixed elevation="0">
-            <v-container class="d-flex justify-space-between">
-              <h1 class="font-weight-light">HOTEL</h1>
-              <div>
-                <v-btn
-                  class="font-weight-thin"
-                  text
-                  v-for="n in menuList"
-                  :key="n"
-                >
-                  {{ n }}
-                </v-btn>
-              </div>
-              <v-btn width="170" height="48" outlined rounded color="white">
-                SIGN UP
-              </v-btn>
-            </v-container>
-          </v-app-bar>
-        </div>
-      </template>
-    </v-img>
-
     <v-main>
       <router-view />
     </v-main>
@@ -33,19 +8,15 @@
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
 
-  data: () => ({
-    menuList: ["About", "Service", "Rooms", "Blog", "Contact"],
-  }),
-};
+  data: () => ({}),
+}
 </script>
 <style lang="scss">
 .fubon-test {
   font-family: adobe-heiti-std, sans-serif;
   font-weight: 400;
   font-style: normal;
-}
-h1 {
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
-  <hello-world />
+  <div>
+    <div class="header">
+      <div>
+        <v-container class="d-flex justify-space-between">
+          <h1 class="font-weight-light">HOTEL</h1>
+          <div>
+            <v-btn class="font-weight-thin" text v-for="n in menuList" :key="n">
+              {{ n }}
+            </v-btn>
+          </div>
+          <v-btn width="170" height="48" outlined rounded color="white">
+            SIGN UP
+          </v-btn>
+        </v-container>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: 'Home',
+  data() {
+    return {
+      menuList: ['About', 'Service', 'Rooms', 'Blog', 'Contact'],
+    }
+  },
+}
 </script>
+<style lang="scss">
+.header {
+  background-image: url('../assets/img/image_background.svg');
+  height: 500px;
+}
+</style>
